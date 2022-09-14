06b16811355116803161a4e39d793839c9985151
pedrobergamini <pedrohgbergamini@gmail.com>
2022-09-14 13:49:30 +1000

    chore: l1 folder basic setup created

---
+++ contracts/l1/StakingBridge.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { IStarkNetCore } from "./interfaces/IStarkNetCore.sol";
+
+contract StakingBridge {
+    IStarkNetCore public starknet;
+
+    function claimRewardsFromL2() external {}
+}
