843490d09c8fe32bd9136ab2c66bb299aa1776a5
pedrobergamini <pedrohgbergamini@gmail.com>
2022-09-21 16:20:00 +1000

    chore: initial l1 <> l2 test setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -34,17 +34,8 @@ const chainIds = {
 };
 
 function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
-  let jsonRpcUrl: string;
-  switch (chain) {
-    case "avalanche":
-      jsonRpcUrl = "https://api.avax.network/ext/bc/C/rpc";
-      break;
-    case "bsc":
-      jsonRpcUrl = "https://bsc-dataseed1.binance.org";
-      break;
-    default:
-      jsonRpcUrl = "https://" + chain + ".infura.io/v3/" + infuraApiKey;
-  }
+  const jsonRpcUrl = "https://" + chain + ".infura.io/v3/" + infuraApiKey;
+
   return {
     accounts: {
       count: 10,
@@ -60,14 +51,8 @@ const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   etherscan: {
     apiKey: {
-      arbitrumOne: process.env.ARBISCAN_API_KEY || "",
-      avalanche: process.env.SNOWTRACE_API_KEY || "",
-      bsc: process.env.BSCSCAN_API_KEY || "",
       goerli: process.env.ETHERSCAN_API_KEY || "",
       mainnet: process.env.ETHERSCAN_API_KEY || "",
-      optimisticEthereum: process.env.OPTIMISM_API_KEY || "",
-      polygon: process.env.POLYGONSCAN_API_KEY || "",
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
     },
   },
   gasReporter: {
@@ -82,6 +67,9 @@ const config: HardhatUserConfig = {
         mnemonic,
       },
       chainId: chainIds.hardhat,
+    },
+    devnet: {
+      url: "http://localhost:5050",
     },
     goerli: getChainConfig("goerli"),
     mainnet: getChainConfig("mainnet"),
@@ -116,6 +104,7 @@ const config: HardhatUserConfig = {
   starknet: {
     // uses current activated venv or global installation
     venv: "active",
+    network: "devnet",
   },
 };
 
