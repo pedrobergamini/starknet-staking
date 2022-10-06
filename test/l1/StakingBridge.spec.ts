ea67a0b80c8ef29ca4ded51e6334507301d2fbee
pedrobergamini <pedrohgbergamini@gmail.com>
2022-10-06 11:38:46 +1100

    chore: update types

--- test/l1/StakingBridge.spec.ts
+++ test/l1/StakingBridge.spec.ts
@@ -7,6 +7,7 @@ const {
 const ONE_MILLION = 1000000;
 const DEFAULT_TIMEOUT = 300000;
 const TOKEN_UNIT = BigInt(10 ** 18);
+const MOCK_STAKING_L2_ADDRESS = 999;
 const networkUrl = "http://127.0.0.1:8545";
 
 describe("StakingBridge", async function () {
@@ -14,55 +15,25 @@ describe("StakingBridge", async function () {
 
   before(async function () {
     const [l1Admin, l1Alice, l1Bob] = await ethers.getSigners();
-    this.starknetMessagingAddress = (await starknet.devnet.loadL1MessagingContract(networkUrl)).address;
-    const l2Admin = await starknet.deployAccount("OpenZeppelin");
-    const l2Alice = await starknet.deployAccount("OpenZeppelin");
-    const l2Bob = await starknet.deployAccount("OpenZeppelin");
-    console.log(l2Admin.starknetContract.address);
-    this.l1Signers = {
+
+    this.signers = {
       admin: l1Admin,
       alice: l1Alice,
       bob: l1Bob,
     };
-    this.l2Signers = {
-      admin: l2Admin,
-      alice: l2Alice,
-      bob: l2Bob,
-    };
     this.StakingBridge = await ethers.getContractFactory("StakingBridge");
-    this.L1ERC20 = await ethers.getContractFactory("ERC20Mock");
-    this.L2ERC20 = await starknet.getContractFactory("contracts/l2/openzeppelin/token/erc20/presets/ERC20");
-    this.RewardsDistribution = await starknet.getContractFactory(
-      "contracts/l2/rewards_distribution/RewardsDistribution",
-    );
-    this.StakingRewards = await starknet.getContractFactory("contracts/l2/staking/StakingRewards");
-    this.starknetMessagingAddress = (await starknet.devnet.loadL1MessagingContract(networkUrl)).address;
+    this.ERC20 = await ethers.getContractFactory("ERC20Mock");
+    this.StarknetMessaging = await ethers.getContractFactory("StarknetMessagingMock");
   });
   beforeEach(async function () {
-    this.l1StakingToken = await this.L1ERC20.deploy("Staking Token", "STK", parseEther(ONE_MILLION.toString()));
-    this.l1RewardToken = await this.L1ERC20.deploy("Reward Token", "RWD", parseEther(ONE_MILLION.toString()));
-    this.l2StakingToken = await this.L2ERC20.deploy({
-      name: BigInt(123),
-      symbol: BigInt(1234),
-      decimals: BigInt(18),
-      initial_supply: { high: BigInt(0), low: BigInt(ONE_MILLION) * TOKEN_UNIT },
-      recipient: BigInt(this.l2Signers.admin.address),
-    });
-    this.rewardsDistribution = await this.RewardsDistribution.deploy({
-      authority: BigInt(this.l2Signers.admin.address),
-      owner: BigInt(this.l2Signers.admin.address),
-    });
-    this.stakingRewards = await this.StakingRewards.deploy({
-      rewards_distribution: this.rewardsDistribution.address,
-      reward_token: BigInt(this.l2RewardToken.address),
-      staking_token: BigInt(this.l2StakingToken.address),
-      owner: BigInt(this.l2Signers.admin.address),
-    });
+    this.stakingToken = await this.ERC20.deploy("Staking Token", "STK", parseEther(ONE_MILLION.toString()));
+    this.rewardToken = await this.ERC20.deploy("Reward Token", "RWD", parseEther(ONE_MILLION.toString()));
+    this.starknetMessaging = await this.StarknetMessaging.deploy();
     this.stakingBridge = await this.StakingBridge.deploy(
-      this.starknetMessagingAddress,
+      this.starknetMessaging.address,
       this.stakingToken.address,
       this.rewardToken.address,
-      BigInt(this.stakingRewards.address),
+      MOCK_STAKING_L2_ADDRESS,
     );
   });
   it("should work", async function () {
