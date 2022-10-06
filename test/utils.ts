30a64c3ef586d5558f879c8d4629d9c700af9797
pedrobergamini <pedrohgbergamini@gmail.com>
2022-10-06 13:50:29 +1100

    chore: refactor utils

--- test/utils.ts
+++ test/utils.ts
@@ -11,9 +11,11 @@ export function fromUint256(value: uint256.Uint256): BigNumber {
   return BigNumber.from(parsedValue.toString());
 }
 
-export const ONE_MILLION = "1000000";
-export const DEFAULT_TIMEOUT = 120000;
-export const MAX_UINT256_STARKNET = toUint256(ethers.constants.MaxUint256);
-export const MAX_UINT256_ETHERS = ethers.constants.MaxUint256;
-export const ONE_DAY = 86400;
-export const SEVEN_DAYS = ONE_DAY * 7;
+export const ONE_MILLION = 1000000;
+export const DEFAULT_TIMEOUT = 300000;
+export const TOKEN_UNIT = BigInt(10 ** 18);
+export const MOCK_STAKING_L2_ADDRESS = 999;
+export const STAKE_AMOUNT = ethers.utils.parseEther("1000");
+export const STARKNET_STAKE_L1_SELECTOR = "0x310825e0f3725d80b141f53c613cfea59901b2fb68ab710e53c39da41c26ca2";
+export const WITHDRAW_MESSAGE = 1;
+export const CLAIM_REWARD_MESSAGE = 2;
